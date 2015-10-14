e57c4a289b9b9466414b607f917eb9727fc7d3cb
Dale Campbell <oshuma@gmail.com>
2015-10-14 13:12:32 -0400

    Initial gem skeleton.

---
+++ lib/sports_south.rb
@@ -0,0 +1,5 @@
+require "sports_south/version"
+
+module SportsSouth
+  # Your code goes here...
+end
---
+++ spec/sports_south_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe SportsSouth do
+  it 'has a version number' do
+    expect(SportsSouth::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
