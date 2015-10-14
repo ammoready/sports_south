a1f66519fc33687e15b18f2c3f535646393b11ff
Dale Campbell <oshuma@gmail.com>
2015-10-14 16:35:13 -0400

    Tweaking option arg names.

--- lib/sports_south/order.rb
+++ lib/sports_south/order.rb
@@ -1,15 +1,16 @@
 module SportsSouth
   class Order < Base
 
-    def initialize(options)
+    def initialize(options = {})
       requires!(options, :username, :password, :source, :customer_number)
+      @options = options
     end
 
-    def add_header(options = {})
+    def add_header(header = {})
       raise 'Not yet implemented.'
     end
 
-    def add_detail(options = {})
+    def add_detail(detail = {})
       raise 'Not yet implemented.'
     end
 
