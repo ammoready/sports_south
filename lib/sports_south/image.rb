faca03aa53500cd280e4757b0c33dd0fb49403f1
Dale Campbell <oshuma@gmail.com>
2015-12-15 12:41:54 -0500

    Adding SportsSouth::Image

---
+++ lib/sports_south/image.rb
@@ -0,0 +1,32 @@
+module SportsSouth
+  class Image < Base
+
+    API_URL = 'http://webservices.theshootingwarehouse.com/smart/images.asmx'
+
+    def self.urls(item_number, options = {})
+      requires!(options, :username, :password, :source, :customer_number)
+
+      http, request = get_http_and_request(API_URL, '/GetPictureURLs')
+
+      request.set_form_data(form_params(options).merge({
+        ItemNumber: item_number
+      }))
+
+      response = http.request(request)
+      body = sanitize_response(response)
+      xml_doc = Nokogiri::XML(body)
+
+      raise SportsSouth::NotAuthenticated if not_authenticated?(xml_doc)
+
+      images = {}
+
+      xml_doc.css('Table').each do |image|
+        size = content_for(image, 'ImageSize').to_sym
+        images[size] = content_for(image, 'Link')
+      end
+
+      images
+    end
+
+  end
+end
