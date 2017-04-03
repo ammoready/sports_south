9b94ad58dba06121dc648ddab17579cc6bd71d0b
David Knight <viacoffee@gmail.com>
2017-04-03 19:29:53 +0000

    Update brand to use new file handling

--- lib/sports_south/brand.rb
+++ lib/sports_south/brand.rb
@@ -6,27 +6,32 @@ module SportsSouth
     def self.all(options = {})
       requires!(options, :username, :password, :source, :customer_number)
 
-      http, request = get_http_and_request(API_URL, '/BrandUpdate')
-
-      request.set_form_data(form_params(options))
-      response = http.request(request)
-      body = sanitize_response(response)
-      xml_doc = Nokogiri::XML(body)
+      form_data = form_params(options)
+      tempfile  = stream_to_tempfile(API_URL, '/BrandUpdate', form_data)
+      xml_doc   = Nokogiri::XML(tempfile)
 
       raise SportsSouth::NotAuthenticated if not_authenticated?(xml_doc)
 
-      brands = []
+      brands = Array.new
 
-      xml_doc.css('Table').each do |brand|
-        brands << {
-          id: content_for(brand, 'BRDNO'),
-          name: content_for(brand, 'BRDNM'),
-          url: content_for(brand, 'BRDURL'),
-          item_count: content_for(brand, 'ITCOUNT'),
-        }
+      SportsSouth::Parser.parse(tempfile, 'Table') do |node|
+        brands.push(self.map_hash(node))
       end
 
+      tempfile.unlink
+
       brands
+    end
+
+    protected
+
+    def self.map_hash(node)
+      {
+        id:         content_for(node, 'BRDNO'),
+        name:       content_for(node, 'BRDNM'),
+        url:        content_for(node, 'BRDURL'),
+        item_count: content_for(node, 'ITCOUNT')
+      }
     end
 
   end
