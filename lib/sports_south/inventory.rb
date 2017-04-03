a05cf95c2a9c1e894c3a73659e028566d58f852a
David Knight <viacoffee@gmail.com>
2017-04-03 18:51:33 +0000

    Cleaning

--- lib/sports_south/inventory.rb
+++ lib/sports_south/inventory.rb
@@ -24,78 +24,23 @@ module SportsSouth
     def self.all(options = {})
       requires!(options, :username, :password, :source, :customer_number)
 
-      options[:last_update] ||= '1/1/1990'  # Return full catalog.
-      options[:last_item]   ||= '-1'  # Return all items.
-
       form_data = form_params(options).merge({
-        LastUpdate: options[:last_update],
-        LastItem: options[:last_item],
+        LastUpdate: options[:last_update] ||= '1/1/1990',
+        LastItem:   options[:last_item]   ||= '-1'
       })
 
-      tempfile = stream_to_tempfile('/DailyItemUpdate', form_data)
-      xml_doc = Nokogiri::XML(tempfile)
+      tempfile  = stream_to_tempfile(API_URL, '/DailyItemUpdate', form_data)
+      xml_doc   = Nokogiri::XML(tempfile)
 
       raise SportsSouth::NotAuthenticated if not_authenticated?(xml_doc)
 
-      items = []
+      items = Array.new
 
-      xml_doc.css('Table').each do |item|
-        items << {
-          item_number: content_for(item, 'ITEMNO'),
-          description: content_for(item, 'IDESC'),
-          manufacturer_sequence: content_for(item, 'IMFSEQ'),
-          manufacturer_number: content_for(item, 'IMFGNO'),
-          catalog_sequence: content_for(item, 'CSEQ'),
-          item_type: ITEM_TYPES[content_for(item, 'ITYPE')],
-          short_description: content_for(item, 'SHDESC'),
-          unit_of_measure: content_for(item, 'UOM'),
-          catalog_price: content_for(item, 'PRC1'),
-          customer_price: content_for(item, 'CPRC'),
-          quantity_on_hand: content_for(item, 'QTYOH'),
-          weight_per_box: content_for(item, 'WTPBX'),
-          upc: content_for(item, 'ITUPC'),
-          manufacturer_item_number: content_for(item, 'MFGINO'),
-          scan_name_1: content_for(item, 'SCNAM1'),
-          scan_name_2: content_for(item, 'SCNAM2'),
-          catalog_code: CATALOG_CODES[content_for(item, 'CATCD')],
-          mapp_price_code: content_for(item, 'MFPRTYP'),
-          mapp_price: content_for(item, 'MFPRC'),
-          category_id: content_for(item, 'CATID'),
-          text_reference_number: content_for(item, 'TXTREF'),
-          picture_reference_number: content_for(item, 'PICREF'),
-          brand_id: content_for(item, 'ITBRDNO'),
-          item_model_number: content_for(item, 'IMODEL'),
-          item_purpose: content_for(item, 'IPURPOSE'),
-          series_description: content_for(item, 'SERIES'),
-          item_length: content_for(item, 'LENGTH'),
-          item_height: content_for(item, 'HEIGHT'),
-          item_width: content_for(item, 'WIDTH'),
-          item_ships_hazmat_air: content_for(item, 'HAZAIR'),
-          item_ships_hazmat_ground: content_for(item, 'HAZGRND'),
-          date_of_last_change: content_for(item, 'CHGDTE'),
-          date_added: content_for(item, 'CHGDTE'),
-          attribute_1: content_for(item, 'ITATR1'),
-          attribute_2: content_for(item, 'ITATR2'),
-          attribute_3: content_for(item, 'ITATR3'),
-          attribute_4: content_for(item, 'ITATR4'),
-          attribute_5: content_for(item, 'ITATR5'),
-          attribute_6: content_for(item, 'ITATR6'),
-          attribute_7: content_for(item, 'ITATR7'),
-          attribute_8: content_for(item, 'ITATR8'),
-          attribute_9: content_for(item, 'ITATR9'),
-          attribute_10: content_for(item, 'ITATR0'),
-          attribute_11: content_for(item, 'ITATR11'),
-          attribute_12: content_for(item, 'ITATR12'),
-          attribute_13: content_for(item, 'ITATR13'),
-          attribute_14: content_for(item, 'ITATR14'),
-          attribute_15: content_for(item, 'ITATR15'),
-          attribute_16: content_for(item, 'ITATR16'),
-          attribute_17: content_for(item, 'ITATR17'),
-          attribute_18: content_for(item, 'ITATR18'),
-          attribute_19: content_for(item, 'ITATR19'),
-          attribute_20: content_for(item, 'ITATR20'),
-        }
+      SportsSouth::Parser.parse(tempfile, 'Table') do |node|
+        items.push(self.map_hash(node))
       end
+
+      tempfile.unlink
 
       items
     end
@@ -105,15 +50,15 @@ module SportsSouth
 
       form_data = form_params(options).merge({
         LastUpdate: options[:last_update] ||= '1/1/1990',
-        LastItem:   options[:last_item] ||= '-1'
+        LastItem:   options[:last_item]   ||= '-1'
       })
 
-      tempfile  = stream_to_tempfile('/DailyItemUpdate', form_data)
+      tempfile  = stream_to_tempfile(API_URL, '/DailyItemUpdate', form_data)
       chunker   = SportsSouth::Chunker.new(size)
 
       chunker.total_count = File.readlines(tempfile).size
 
-      SportsSouth::Parser.chunk(tempfile) do |chunk|
+      SportsSouth::Parser.parse(tempfile, 'Table') do |node|
         if chunker.is_full?
           yield(chunker.chunk)
 
@@ -123,9 +68,11 @@ module SportsSouth
 
           break
         else
-          chunker.add(chunk)
+          chunker.add(self.map_hash(node))
         end
       end
+
+      tempfile.unlink
     end
 
     def self.get_text(item_number, options = {})
@@ -252,21 +199,62 @@ module SportsSouth
 
     protected
 
-    def self.stream_to_tempfile(endpoint, form_data)
-      temp_file     = Tempfile.new
-      http, request = get_http_and_request(API_URL, endpoint)
-
-      request.set_form_data(form_data)
-
-      http.request(request) do |response|
-        File.open(temp_file, 'w') do |file|
-          response.read_body do |chunk|
-            file.write(chunk.gsub('&lt;', '<').gsub('&gt;', '>'))
-          end
-        end
-      end
-
-      temp_file
+    def self.map_hash(node)
+      {
+        item_number:              content_for(node, 'ITEMNO'),
+        description:              content_for(node, 'IDESC'),
+        manufacturer_sequence:    content_for(node, 'IMFSEQ'),
+        manufacturer_number:      content_for(node, 'IMFGNO'),
+        catalog_sequence:         content_for(node, 'CSEQ'),
+        item_type:                ITEM_TYPES[content_for(node, 'ITYPE')],
+        short_description:        content_for(node, 'SHDESC'),
+        unit_of_measure:          content_for(node, 'UOM'),
+        catalog_price:            content_for(node, 'PRC1'),
+        customer_price:           content_for(node, 'CPRC'),
+        quantity_on_hand:         content_for(node, 'QTYOH'),
+        weight_per_box:           content_for(node, 'WTPBX'),
+        upc:                      content_for(node, 'ITUPC'),
+        manufacturer_item_number: content_for(node, 'MFGINO'),
+        scan_name_1:              content_for(node, 'SCNAM1'),
+        scan_name_2:              content_for(node, 'SCNAM2'),
+        catalog_code:             CATALOG_CODES[content_for(node, 'CATCD')],
+        mapp_price_code:          content_for(node, 'MFPRTYP'),
+        mapp_price:               content_for(node, 'MFPRC'),
+        category_id:              content_for(node, 'CATID'),
+        text_reference_number:    content_for(node, 'TXTREF'),
+        picture_reference_number: content_for(node, 'PICREF'),
+        brand_id:                 content_for(node, 'ITBRDNO'),
+        item_model_number:        content_for(node, 'IMODEL'),
+        item_purpose:             content_for(node, 'IPURPOSE'),
+        series_description:       content_for(node, 'SERIES'),
+        item_length:              content_for(node, 'LENGTH'),
+        item_height:              content_for(node, 'HEIGHT'),
+        item_width:               content_for(node, 'WIDTH'),
+        item_ships_hazmat_air:    content_for(node, 'HAZAIR'),
+        item_ships_hazmat_ground: content_for(node, 'HAZGRND'),
+        date_of_last_change:      content_for(node, 'CHGDTE'),
+        date_added:               content_for(node, 'CHGDTE'),
+        attribute_1:              content_for(node, 'ITATR1'),
+        attribute_2:              content_for(node, 'ITATR2'),
+        attribute_3:              content_for(node, 'ITATR3'),
+        attribute_4:              content_for(node, 'ITATR4'),
+        attribute_5:              content_for(node, 'ITATR5'),
+        attribute_6:              content_for(node, 'ITATR6'),
+        attribute_7:              content_for(node, 'ITATR7'),
+        attribute_8:              content_for(node, 'ITATR8'),
+        attribute_9:              content_for(node, 'ITATR9'),
+        attribute_10:             content_for(node, 'ITATR0'),
+        attribute_11:             content_for(node, 'ITATR11'),
+        attribute_12:             content_for(node, 'ITATR12'),
+        attribute_13:             content_for(node, 'ITATR13'),
+        attribute_14:             content_for(node, 'ITATR14'),
+        attribute_15:             content_for(node, 'ITATR15'),
+        attribute_16:             content_for(node, 'ITATR16'),
+        attribute_17:             content_for(node, 'ITATR17'),
+        attribute_18:             content_for(node, 'ITATR18'),
+        attribute_19:             content_for(node, 'ITATR19'),
+        attribute_20:             content_for(node, 'ITATR20')
+      }
     end
 
   end
