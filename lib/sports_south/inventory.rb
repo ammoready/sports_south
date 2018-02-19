d01b433c62aa571ad8365e2cb7af24d099519d69
David Knight <viacoffee@gmail.com>
2018-02-19 11:24:54 -0500

    inventory quantity methods

--- lib/sports_south/inventory.rb
+++ lib/sports_south/inventory.rb
@@ -55,6 +55,50 @@ module SportsSouth
       end
     end
 
+    def self.quantity(chunk_size = 100, options = {}, &block)
+      requires!(options, :username, :password)
+
+      if options[:last_updated].present?
+        options[:last_updated] = options[:last_updated].to_s("yyyy-MM-ddTHH:mm:sszzz")
+      else
+        options[:last_updated] = '1990-09-25T14:15:47-04:00'
+      end
+
+      options[:last_item] ||= '-1'
+
+      new(options).all(chunk_size, &block)
+    end
+
+    def quantity(chunk_size, &block)
+      chunker = SportsSouth::Chunker.new(chunk_size)
+      http, request = get_http_and_request(API_URL, '/IncrementalOnhandUpdate')
+
+      request.set_form_data(form_params = form_params(@options).merge({
+        SinceDateTime: @options[:last_updated],
+        LastItem:      @options[:last_item].to_s
+      }))
+
+      response  = http.request(request)
+      xml_doc   = Nokogiri::XML(sanitize_response(response))
+
+      xml_doc.css('Onhand').map do |item|
+        if chunker.is_full?
+          yield(chunker.chunk)
+
+          chunker.reset!
+        else
+          chunker.add({
+            item_identifier: content_for(node, 'I'),
+            quantity: content_for(node, 'Q').to_i
+          })
+        end
+      end
+
+      if chunker.chunk.count > 0
+        yield(chunker.chunk)
+      end
+    end
+
     def get(item_identifier)
       http, request = get_http_and_request(API_URL, '/OnhandInquiry')
 
