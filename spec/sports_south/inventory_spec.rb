c586582ebdb151d55900bd0e637a2ed7fd918945
Justin Wilson <justin@jwils.co>
2018-07-23 09:07:50 -0400

    Update inventory test

--- spec/sports_south/inventory_spec.rb
+++ spec/sports_south/inventory_spec.rb
@@ -16,6 +16,20 @@ describe SportsSouth::Inventory do
       count = 0
       SportsSouth::Inventory.all(credentials) do |item|
         count += 1
+        case count
+        when 1
+          expect(item[:item_identifier]).to eq('50001')
+          expect(item[:quantity]).to        eq(25)
+          expect(item[:price]).to           eq('11.89')
+        when 2
+          expect(item[:item_identifier]).to eq('50002')
+          expect(item[:quantity]).to        eq(5)
+          expect(item[:price]).to           eq('110.99')
+        when 50
+          expect(item[:item_identifier]).to eq('50050')
+          expect(item[:quantity]).to        eq(10)
+          expect(item[:price]).to           eq('310.62')
+        end
       end
 
       expect(count).to eq(50)
