5dd9572a91af870443cabb0f417278135d791932
Pedro Pinheiro <pmop@protonmail.com>
2026-05-07 15:41:02 -0300

    Daily item count

--- lib/sports_south/inventory.rb
+++ lib/sports_south/inventory.rb
@@ -102,6 +102,12 @@ module SportsSouth
       new(options).all
     end
 
+    def self.daily_item_count(options = {})
+      requires!(options, :username, :password, :last_update)
+
+      new(options).daily_item_count
+    end
+
     def get(item_identifier)
       http, request = get_http_and_request(API_URL, '/OnhandInquiry')
 
@@ -109,6 +115,21 @@ module SportsSouth
 
       response = http.request(request)
       xml_doc  = Nokogiri::XML(sanitize_response(response))
+    end
+
+    def daily_item_count
+      http, request = get_http_and_request(API_URL, '/DailyItemCount')
+
+      last_update_formatted = @options[:last_update].strftime('%m/%d/%Y')
+
+      request.set_form_data(form_params(@options).merge({
+        LastUpdate: last_update_formatted
+      }))
+
+      response = http.request(request)
+      xml_doc  = Nokogiri::XML(sanitize_response(response))
+
+      content_for(xml_doc, 'int').to_i
     end
 
     protected
