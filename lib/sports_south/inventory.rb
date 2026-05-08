8c5195c063c483a304cdd9cbce15dfce3e26ae60
Pedro Pinheiro <pmop@protonmail.com>
2026-05-08 09:34:22 -0300

    Format

--- lib/sports_south/inventory.rb
+++ lib/sports_south/inventory.rb
@@ -5,145 +5,144 @@ module SportsSouth
     ITEM_NODE_NAME = 'Onhand'
 
     def initialize(options = {})
-    requires!(options, :username, :password)
+      requires!(options, :username, :password)
 
-    @options = options
+      @options = options
+    end
+
+    def self.get_quantity_file(options = {})
+      requires!(options, :username, :password)
+
+      options[:last_updated] = '1990-09-25T14:15:47-04:00'
+      options[:last_item]    = '-1'
+
+      new(options).get_quantity_file
+    end
+
+    def self.all(options = {})
+      requires!(options, :username, :password)
+
+      if options[:last_updated].present?
+        options[:last_updated] = options[:last_updated].strftime('%Y-%m-%dT%H:%M:00.00%:z')
+      else
+        options[:last_updated] = '1990-09-25T14:15:47-04:00'
+      end
+
+      options[:last_item] ||= '-1'
+
+      new(options).all
+    end
+
+    def self.get(item_identifier, options = {})
+      requires!(options, :username, :password)
+      new(options).get(item_identifier)
+    end
+
+    def all
+      http, request = get_http_and_request(API_URL, '/IncrementalOnhandUpdate')
+
+      request.set_form_data(form_params = form_params(@options).merge({
+        SinceDateTime: @options[:last_updated],
+        LastItem:      @options[:last_item].to_s
+      }))
+
+      items    = []
+      tempfile = download_to_tempfile(http, request)
+
+      tempfile.rewind
+
+      Nokogiri::XML::Reader.from_io(tempfile).each do |reader|
+        next unless reader.node_type == Nokogiri::XML::Reader::TYPE_ELEMENT
+        next unless reader.name == ITEM_NODE_NAME
+
+        node = Nokogiri::XML.parse(reader.outer_xml)
+
+        _map_hash = map_hash(node.css(ITEM_NODE_NAME))
+
+        items << _map_hash unless _map_hash.nil?
+      end
+
+      tempfile.close
+      tempfile.unlink
+
+      items
+    end
+
+    def get_quantity_file
+      tempfile      = Tempfile.new
+      http, request = get_http_and_request(API_URL, '/IncrementalOnhandUpdate')
+
+      request.set_form_data(form_params = form_params(@options).merge({
+        SinceDateTime: @options[:last_updated],
+        LastItem:      @options[:last_item].to_s
+      }))
+
+      response = http.request(request)
+      xml_doc  = Nokogiri::XML(sanitize_response(response))
+
+      xml_doc.css('Onhand').map do |item|
+        tempfile.puts("#{content_for(item, 'I')},#{content_for(item, 'Q')}")
+      end
+
+      tempfile.close
+      tempfile.path
+    end
+
+    def self.quantity(options = {})
+      requires!(options, :username, :password)
+
+      if options[:last_updated].present?
+        options[:last_updated] = options[:last_updated].to_s("yyyy-MM-ddTHH:mm:sszzz")
+      else
+        options[:last_updated] = '1990-09-25T14:15:47-04:00'
+      end
+
+      options[:last_item] ||= '-1'
+
+      new(options).all
+    end
+
+    def self.daily_item_count(options = {})
+      requires!(options, :username, :password, :last_update)
+
+      new(options).daily_item_count
+    end
+
+    def get(item_identifier)
+      http, request = get_http_and_request(API_URL, '/OnhandInquiry')
+
+      request.set_form_data(form_params(@options).merge({ ItemNumber: item_identifier }))
+
+      response = http.request(request)
+      xml_doc  = Nokogiri::XML(sanitize_response(response))
+    end
+
+    def daily_item_count
+      http, request = get_http_and_request(API_URL, '/DailyItemCount')
+      last_update_formatted = @options[:last_update]
+
+      if last_update_formatted.is_a?(Date)
+        last_update_formatted = last_update_formatted.strftime('%m/%d/%Y')
+      end
+
+      request.set_form_data(form_params(@options).merge({
+        LastUpdate: last_update_formatted
+      }))
+
+      response = http.request(request)
+      xml_doc  = Nokogiri::XML(sanitize_response(response))
+
+      content_for(xml_doc, 'int').to_i
+    end
+
+    protected
+
+    def map_hash(node)
+      {
+        item_identifier: content_for(node, 'I'),
+        quantity: content_for(node, 'Q').to_i,
+        price: content_for(node, 'C')
+      }
+    end
   end
-
-  def self.get_quantity_file(options = {})
-  requires!(options, :username, :password)
-
-  options[:last_updated] = '1990-09-25T14:15:47-04:00'
-  options[:last_item]    = '-1'
-
-  new(options).get_quantity_file
-end
-
-def self.all(options = {})
-requires!(options, :username, :password)
-
-if options[:last_updated].present?
-  options[:last_updated] = options[:last_updated].strftime('%Y-%m-%dT%H:%M:00.00%:z')
-else
-  options[:last_updated] = '1990-09-25T14:15:47-04:00'
-end
-
-options[:last_item] ||= '-1'
-
-new(options).all
-end
-
-def self.get(item_identifier, options = {})
-requires!(options, :username, :password)
-new(options).get(item_identifier)
-end
-
-def all
-  http, request = get_http_and_request(API_URL, '/IncrementalOnhandUpdate')
-
-  request.set_form_data(form_params = form_params(@options).merge({
-  SinceDateTime: @options[:last_updated],
-  LastItem:      @options[:last_item].to_s
-  }))
-
-  items    = []
-  tempfile = download_to_tempfile(http, request)
-
-  tempfile.rewind
-
-  Nokogiri::XML::Reader.from_io(tempfile).each do |reader|
-    next unless reader.node_type == Nokogiri::XML::Reader::TYPE_ELEMENT
-    next unless reader.name == ITEM_NODE_NAME
-
-    node = Nokogiri::XML.parse(reader.outer_xml)
-
-    _map_hash = map_hash(node.css(ITEM_NODE_NAME))
-
-    items << _map_hash unless _map_hash.nil?
-  end
-
-  tempfile.close
-  tempfile.unlink
-
-  items
-end
-
-def get_quantity_file
-  tempfile      = Tempfile.new
-  http, request = get_http_and_request(API_URL, '/IncrementalOnhandUpdate')
-
-  request.set_form_data(form_params = form_params(@options).merge({
-  SinceDateTime: @options[:last_updated],
-  LastItem:      @options[:last_item].to_s
-  }))
-
-  response = http.request(request)
-  xml_doc  = Nokogiri::XML(sanitize_response(response))
-
-  xml_doc.css('Onhand').map do |item|
-    tempfile.puts("#{content_for(item, 'I')},#{content_for(item, 'Q')}")
-  end
-
-  tempfile.close
-  tempfile.path
-end
-
-def self.quantity(options = {})
-requires!(options, :username, :password)
-
-if options[:last_updated].present?
-  options[:last_updated] = options[:last_updated].to_s("yyyy-MM-ddTHH:mm:sszzz")
-else
-  options[:last_updated] = '1990-09-25T14:15:47-04:00'
-end
-
-options[:last_item] ||= '-1'
-
-new(options).all
-end
-
-def self.daily_item_count(options = {})
-requires!(options, :username, :password, :last_update)
-
-new(options).daily_item_count
-end
-
-def get(item_identifier)
-  http, request = get_http_and_request(API_URL, '/OnhandInquiry')
-
-  request.set_form_data(form_params(@options).merge({ ItemNumber: item_identifier }))
-
-  response = http.request(request)
-  xml_doc  = Nokogiri::XML(sanitize_response(response))
-end
-
-def daily_item_count
-  http, request = get_http_and_request(API_URL, '/DailyItemCount')
-  last_update_formatted = @options[:last_update]
-
-  if last_update_formatted.is_a?(Date)
-    last_update_formatted = last_update_formatted.strftime('%m/%d/%Y')
-  end
-
-  request.set_form_data(form_params(@options).merge({
-  LastUpdate: last_update_formatted
-  }))
-
-  response = http.request(request)
-  xml_doc  = Nokogiri::XML(sanitize_response(response))
-
-  content_for(xml_doc, 'int').to_i
-end
-
-protected
-
-def map_hash(node)
-  {
-  item_identifier: content_for(node, 'I'),
-  quantity: content_for(node, 'Q').to_i,
-  price: content_for(node, 'C')
-}
-end
-
-end
 end
